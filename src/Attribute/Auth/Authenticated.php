1da366557d722fd928b8895d548312588a54dd94
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-08-09 17:43:01 +0300

    feat(attribute): add Authenticated and AnonymousOnly attributes

---
+++ src/Attribute/Auth/Authenticated.php
@@ -0,0 +1,29 @@
+<?php
+// ============================================================================
+// File:    Authenticated.php
+// Author:  Recep Seymen Konuk <konukrecepseymen@gmail.com>
+//
+// Licensed under the terms of the LICENSE file in the project root directory.
+// ============================================================================
+
+namespace Seymenkonuk\Framework\Attribute;
+
+
+use Attribute;
+
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+class Authenticated
+{
+    /**
+     * Route'un yalnızca kimliği doğrulanmış kullanıcılar tarafından
+     * erişilebileceğini belirtir.
+     *
+     * Sınıf seviyesinde tanımlandığında sınıf içerisindeki tüm route'lara
+     * Metot seviyesinde tanımlandığında yalnızca ilgili route'a uygulanır.
+     *
+     * Sınıf ve metot seviyesinde farklı bir authentication kuralı tanımlanmışsa
+     * metot seviyesinde tanımlanan kural geçerli olur.
+     */
+    public function __construct() {}
+}
