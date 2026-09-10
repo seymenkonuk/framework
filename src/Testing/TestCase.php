e15602e41622f60e9b758c2070a1529268617c98
Recep Seymen Konuk <konukrecepseymen@gmail.com>
2026-09-10 20:31:11 +0300

    feat(testing): implement TestCase request methods

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -12,6 +12,7 @@ namespace Seymenkonuk\Framework\Testing;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 use Seymenkonuk\Framework\Application;
+use Seymenkonuk\Framework\Http\Request\Request;
 use Seymenkonuk\Framework\Http\Response\ResponseState;
 
 
@@ -164,7 +165,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function get(string $uri): ResponseState;
+    protected function get(string $uri): ResponseState
+    {
+        return $this->formRequest("GET", $uri, []);
+    }
 
     /**
      * HTTP QUERY request gönderir.
@@ -174,7 +178,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function query(string $uri, array $data): ResponseState;
+    protected function query(string $uri, array $data): ResponseState
+    {
+        return $this->formRequest("QUERY", $uri, $data);
+    }
 
     /**
      * HTTP POST request gönderir.
@@ -184,7 +191,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function post(string $uri, array $data): ResponseState;
+    protected function post(string $uri, array $data): ResponseState
+    {
+        return $this->formRequest("POST", $uri, $data);
+    }
 
     /**
      * HTTP PUT request gönderir.
@@ -194,7 +204,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function put(string $uri, array $data): ResponseState;
+    protected function put(string $uri, array $data): ResponseState
+    {
+        return $this->formRequest("PUT", $uri, $data);
+    }
 
     /**
      * HTTP PATCH request gönderir.
@@ -204,7 +217,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function patch(string $uri, array $data): ResponseState;
+    protected function patch(string $uri, array $data): ResponseState
+    {
+        return $this->formRequest("PATCH", $uri, $data);
+    }
 
     /**
      * HTTP DELETE request gönderir.
@@ -214,7 +230,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function delete(string $uri, array $data): ResponseState;
+    protected function delete(string $uri, array $data): ResponseState
+    {
+        return $this->formRequest("DELETE", $uri, $data);
+    }
 
     // --------------------------------------------------------------------------
     // JSON REQUEST
@@ -227,7 +246,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function getJson(string $uri): ResponseState;
+    protected function getJson(string $uri): ResponseState
+    {
+        return $this->jsonRequest("GET", $uri, []);
+    }
 
     /**
      * JSON formatında HTTP QUERY request gönderir.
@@ -237,7 +259,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function queryJson(string $uri, array $data): ResponseState;
+    protected function queryJson(string $uri, array $data): ResponseState
+    {
+        return $this->jsonRequest("QUERY", $uri, $data);
+    }
 
     /**
      * JSON formatında HTTP POST request gönderir.
@@ -247,7 +272,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function postJson(string $uri, array $data): ResponseState;
+    protected function postJson(string $uri, array $data): ResponseState
+    {
+        return $this->jsonRequest("POST", $uri, $data);
+    }
 
     /**
      * JSON formatında HTTP PUT request gönderir.
@@ -257,7 +285,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function putJson(string $uri, array $data): ResponseState;
+    protected function putJson(string $uri, array $data): ResponseState
+    {
+        return $this->jsonRequest("PUT", $uri, $data);
+    }
 
     /**
      * JSON formatında HTTP PATCH request gönderir.
@@ -267,7 +298,10 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function patchJson(string $uri, array $data): ResponseState;
+    protected function patchJson(string $uri, array $data): ResponseState
+    {
+        return $this->jsonRequest("PATCH", $uri, $data);
+    }
 
     /**
      * JSON formatında HTTP DELETE request gönderir.
@@ -277,5 +311,126 @@ abstract class TestCase extends PHPUnitTestCase
      *
      * @return ResponseState oluşturulan response state.
      */
-    abstract protected function deleteJson(string $uri, array $data): ResponseState;
+    protected function deleteJson(string $uri, array $data): ResponseState
+    {
+        return $this->jsonRequest("DELETE", $uri, $data);
+    }
+
+    // --------------------------------------------------------------------------
+    // INTERNAL
+    // --------------------------------------------------------------------------
+
+    /**
+     * URI'den path bilgisini döndürür.
+     *
+     * @param string $uri request URI'si.
+     *
+     * @return string URI'nin path kısmı.
+     */
+    private function pathFromUri(string $uri): string
+    {
+        return parse_url("https://recepseymenkonuk.com" . $uri, PHP_URL_PATH) ?: "/";
+    }
+
+    /**
+     * URI'den query parametrelerini döndürür.
+     *
+     * @param string $uri request URI'si.
+     *
+     * @return array<string, mixed> URI içerisindeki query parametreleri.
+     */
+    private function queryFromUri(string $uri): array
+    {
+        $query = parse_url("https://recepseymenkonuk.com" . $uri, PHP_URL_QUERY);
+
+        if ($query === null || $query === "" || $query === false) {
+            return [];
+        }
+
+        parse_str($query, $data);
+
+        /** @phpstan-ignore return.type */
+        return $data;
+    }
+
+    /**
+     * Form verilerini URL encoded string'e dönüştürür.
+     *
+     * @param array<string, mixed> $data form verileri.
+     *
+     * @return string URL encoded form verisi.
+     */
+    private function encodeFormData(array $data): string
+    {
+        return http_build_query($data);
+    }
+
+    /**
+     * Verileri JSON string'ine dönüştürür.
+     *
+     * @param array<string, mixed> $data JSON'a dönüştürülecek veriler.
+     *
+     * @return string JSON verisi.
+     */
+    private function encodeJsonData(array $data): string
+    {
+        return json_encode(
+            $data,
+            JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_THROW_ON_ERROR,
+        );
+    }
+
+    /**
+     * Form verisi içeren request gönderir.
+     *
+     * @param string $method HTTP request metodu.
+     * @param string $uri request URI'si.
+     * @param array<string, mixed> $data gönderilecek request verileri.
+     *
+     * @return ResponseState oluşturulan response state.
+     */
+    private function formRequest(string $method, string $uri, array $data): ResponseState
+    {
+        return $this->application()
+            ->run(new Request(
+                version: "HTTP 1/1",
+                method: $method,
+                path: $this->pathFromUri($uri),
+                headers: $this->headers,
+                cookies: $this->cookies,
+                body: $this->encodeFormData($data),
+                post: $data,
+                queries: $this->queryFromUri($uri),
+                files: [],
+                params: [],
+                server: $this->servers,
+            ), false);
+    }
+
+    /**
+     * JSON verisi içeren request gönderir.
+     *
+     * @param string $method HTTP request metodu.
+     * @param string $uri request URI'si.
+     * @param array<string, mixed> $data gönderilecek request verileri.
+     *
+     * @return ResponseState oluşturulan response state.
+     */
+    private function jsonRequest(string $method, string $uri, array $data): ResponseState
+    {
+        return $this->application()
+            ->run(new Request(
+                version: "HTTP 1/1",
+                method: $method,
+                path: $this->pathFromUri($uri),
+                headers: $this->headers,
+                cookies: $this->cookies,
+                body: $this->encodeJsonData($data),
+                post: [],
+                queries: $this->queryFromUri($uri),
+                files: [],
+                params: [],
+                server: $this->servers,
+            ), false);
+    }
 }
